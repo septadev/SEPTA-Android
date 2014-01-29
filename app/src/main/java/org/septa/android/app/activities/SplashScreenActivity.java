844451c3315ffc3063df7f75b42166c343b36be1
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-01-29 13:00:37 -0500

    adding the splash screen class, needs implementation

---
+++ app/src/main/java/org/septa/android/app/activities/SplashScreenActivity.java
@@ -0,0 +1,11 @@
+/*
+ * SplashScreenActivity.java
+ * Last modified on 01-29-2014 12:56-0500 by brianhmayo
+ *
+ * Copyright (c) 2014 SEPTA.  All rights reserved.
+ */
+
+package org.septa.android.app.activities;
+
+public class SplashScreenActivity {
+}

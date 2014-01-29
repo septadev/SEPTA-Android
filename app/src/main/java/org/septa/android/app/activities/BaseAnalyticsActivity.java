969b131eda1aacc7fec6e4f88aaa573d03ae100e
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-01-29 12:55:53 -0500

    adding the shell of the base analytics activity and action bar activity for analytics take off injection.

---
+++ app/src/main/java/org/septa/android/app/activities/BaseAnalyticsActivity.java
@@ -0,0 +1,19 @@
+package org.septa.android.app.activities;
+
+import android.app.Activity;
+
+public class BaseAnalyticsActivity extends Activity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // placeholder for analytics code
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // placeholder for analytics code
+    }
+}

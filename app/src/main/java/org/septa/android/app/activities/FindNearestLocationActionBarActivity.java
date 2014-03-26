2f498520c17c193a85629a860876b2052d8643c4
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-03-26 17:29:20 -0400

    tapping into the location manager via LocationClient from the MapFragment to get the user's locaiton and setup the location listener.

--- app/src/main/java/org/septa/android/app/activities/FindNearestLocationActionBarActivity.java
+++ app/src/main/java/org/septa/android/app/activities/FindNearestLocationActionBarActivity.java
@@ -7,11 +7,47 @@
 
 package org.septa.android.app.activities;
 
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 import org.septa.android.app.R;
 
-public class FindNearestLocationActionBarActivity extends BaseAnalyticsActionBarActivity {
+public class FindNearestLocationActionBarActivity extends BaseAnalyticsActionBarActivity implements
+        LocationListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
     public static final String TAG = FindNearestLocationActionBarActivity.class.getName();
+
+    private GoogleMap map;
+
+    final int RQS_GooglePlayServices = 1;
+
+    LocationClient mLocationClient;
+
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +61,115 @@ public class FindNearestLocationActionBarActivity extends BaseAnalyticsActionBar
         getSupportActionBar().setIcon(R.drawable.ic_actionbar_findnearestlocation);
         getSupportActionBar().setTitle(titleText);
 
+        FragmentManager myFragmentManager = getSupportFragmentManager();
+        SupportMapFragment mySupportMapFragment
+                = (SupportMapFragment)myFragmentManager.findFragmentById(R.id.nearestLocationMapFragment);
+        map = mySupportMapFragment.getMap();
+
+        map.setMyLocationEnabled(true);
+
+        mLocationClient = new LocationClient(this, this, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        Log.d(TAG, "heard a location change");
+        Log.d(TAG, "new location lat "+newLocation.getLatitude());
+        Log.d(TAG, "new location lon "+newLocation.getLongitude());
+        Log.d(TAG, "with location accuracy" + newLocation.getAccuracy());
+
+        LatLng currentLocation = new LatLng(newLocation.getLatitude(), newLocation.getLongitude());
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, Float.parseFloat(getString(R.string.map_zoom_level_float))));
+    }
+
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Log.d(TAG, "location services connected");
+
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Log.d(TAG, "location services disconnected.");
+    }
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        9000);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.d(TAG, "location services error: " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (resultCode != ConnectionResult.SUCCESS){
+
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices);
+        }
     }
 }

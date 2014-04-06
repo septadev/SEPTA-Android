10dd62d2430e6440629e75d8a2b6db6fd81ee875
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-04-06 11:55:45 -0400

    created the trainview and included it in realtime with its map fragment and actionbar activity.  it is a stub at the moment but is in the process of being filled out.

--- app/src/main/java/org/septa/android/app/activities/TrainViewActionBarActivity.java
+++ app/src/main/java/org/septa/android/app/activities/TrainViewActionBarActivity.java
@@ -1,4 +1,34 @@
+/*
+ * TrainViewActionBarActivity.java
+ * Last modified on 04-03-2014 16:41-0400 by brianhmayo
+ *
+ * Copyright (c) 2014 SEPTA.  All rights reserved.
+ */
+
 package org.septa.android.app.activities;
 
-public class TrainViewActionBarActivity {
+import android.os.Bundle;
+
+import org.septa.android.app.R;
+
+public class TrainViewActionBarActivity extends BaseAnalyticsActionBarActivity {
+    public static final String TAG = TrainViewActionBarActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String actionBarTitleText = getIntent().getStringExtra(getString(R.string.actionbar_titletext_key));
+        String iconImageNameSuffix = getIntent().getStringExtra(getString(R.string.actionbar_iconimage_imagenamesuffix_key));
+
+        String resourceName = getString(R.string.actionbar_iconimage_imagename_base).concat(iconImageNameSuffix);
+
+        int id = getResources().getIdentifier(resourceName, "drawable", getPackageName());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(actionBarTitleText);
+        getSupportActionBar().setIcon(id);
+
+        setContentView(R.layout.trainview);
+    }
 }
--- app/src/main/java/org/septa/android/app/fragments/TrainViewMapFragment.java
+++ app/src/main/java/org/septa/android/app/fragments/TrainViewMapFragment.java
@@ -1,4 +1,14 @@
+/*
+ * TrainViewMapFragment.java
+ * Last modified on 04-03-2014 16:49-0400 by brianhmayo
+ *
+ * Copyright (c) 2014 SEPTA.  All rights reserved.
+ */
+
 package org.septa.android.app.fragments;
 
-public class TrainViewMapFragment {
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class TrainViewMapFragment extends SupportMapFragment {
+
 }

d1da9cf888932fa62bd042e9b49ae9e42a3d9f76
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-05-21 08:54:13 -0400

    fixed a misspelling in an element for the Alerts feed.

--- app/src/main/java/org/septa/android/app/models/servicemodels/AlertModel.java
+++ app/src/main/java/org/septa/android/app/models/servicemodels/AlertModel.java
@@ -18,7 +18,7 @@ public class AlertModel implements Comparable<AlertModel> {
     @SerializedName("isadvisory") private String isAdvisory;
     @SerializedName("isalert") private String isAlert;
     @SerializedName("isdetour") private String isDetour;
-    @SerializedName("issuspend") private String isSuspended;
+    @SerializedName("issuppend") private String isSuspended;
     @SerializedName("last_update") private Date lastUpdate;
     @SerializedName("mode") private String mode;
     @SerializedName("route_id") private String routeId;

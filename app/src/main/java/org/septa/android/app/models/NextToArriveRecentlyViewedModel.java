c20ad4fbe62b63d25f3778cb0e6cd11936e24a39
Brian H. Mayo <brian.h.mayo@gmail.com>
2014-06-06 12:56:58 -0400

    adding new classes to support the different types of rows in next to arrive.

---
+++ app/src/main/java/org/septa/android/app/models/NextToArriveRecentlyViewedModel.java
@@ -0,0 +1,11 @@
+/*
+ * NextToArriveRecentlyViewedModel.java
+ * Last modified on 06-06-2014 11:20-0400 by brianhmayo
+ *
+ * Copyright (c) 2014 SEPTA.  All rights reserved.
+ */
+
+package org.septa.android.app.models;
+
+public class NextToArriveRecentlyViewedModel extends NextToArriveStoredTripModel {
+}

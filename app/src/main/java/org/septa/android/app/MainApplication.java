bade9e83f1076ff44f2c54f3e4e344d57536c619
Antoine Campbell <acampbell@captechconsulting.com>
2014-07-26 15:10:00 -0400

    - Removed unused database loading resource values - Database now only loads when not present or needs to upgrading via the database version - Added SETPA Crashlytics key - Crashlytics disabled for debug builds, reduce noise when testing locally - Database uses force upgrade option as there are no update scripts and a read-only db is uesd - Updated version

--- app/src/main/java/org/septa/android/app/MainApplication.java
+++ app/src/main/java/org/septa/android/app/MainApplication.java
@@ -1,21 +1,33 @@
+/**
+ * Created by Trey Robinson on 7/10/14.
+ *
+ * Copyright (c) 2014 SEPTA.  All rights reserved.
+ */
+
 package org.septa.android.app;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
 import org.septa.android.app.managers.AlertManager;
 import org.septa.android.app.managers.SharedPreferencesManager;
 
-/**
- * Created by Trey Robinson on 7/10/14.
- */
 public class MainApplication extends Application{
+
+    private static final String TAG = "MainApplication";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Crashlytics.start(this);
+        // Disable Crashlytics for debug builds (unsigned)
+        if(!BuildConfig.DEBUG) {
+            Crashlytics.start(this);
+            Log.i(TAG, "Starting Crashlytics");
+        } else {
+            Log.i(TAG, "Crashlytics disabled for DEBUG builds");
+        }
         SharedPreferencesManager.getInstance().init(this);
 
         //@TODO putting timing reqs around this and load on demand

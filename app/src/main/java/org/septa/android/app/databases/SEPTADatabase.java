e615714c163bebba7364bc0becd52c01ce19aa0b
Antoine Campbell <acampbell@captechconsulting.com>
2014-09-11 19:36:42 -0400

    - Database update to resolve issue with GTFS data

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
 
     public SEPTADatabase(Context context) {
 

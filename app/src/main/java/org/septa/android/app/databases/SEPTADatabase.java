4abebfde88ec20222ee5b555befd36c6c1400032
Antoine Campbell <acampbell@captechconsulting.com>
2015-01-11 13:20:45 -0500

    - Database updated - Updated build number

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 12;
+    private static final int DATABASE_VERSION = 13;
 
     public SEPTADatabase(Context context) {
 

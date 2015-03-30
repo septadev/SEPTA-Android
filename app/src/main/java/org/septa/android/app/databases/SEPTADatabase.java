85f36e28211fb5ffbcd05475e768f4eeb1c865d7
Antoine Campbell <acampbell@captechconsulting.com>
2015-03-30 12:43:22 -0400

    - Updated version numbers - Added new DB - Bug fix for transit view using incorrect serviceId to determine service status. Optimization coming for this bug fix.

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 17;
+    private static final int DATABASE_VERSION = 18;
 
     public SEPTADatabase(Context context) {
 

9e6dbca6b79a26da19e5ccaf09ddca6068da89f4
John Hunchar <jhunchar@captechconsulting.com>
2015-06-05 09:23:43 -0400

    Updated database for next release

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 19;
+    private static final int DATABASE_VERSION = 20;
 
     public SEPTADatabase(Context context) {
 

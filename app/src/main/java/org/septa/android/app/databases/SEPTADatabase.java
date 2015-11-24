7597e8b2745c7d031322202368a2fcf16a698b70
yannick-b <ybaudru@septa.org>
2015-11-24 14:49:30 -0500

    new route 45 , split route 23

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 29;
+    private static final int DATABASE_VERSION = 30;
 
     public SEPTADatabase(Context context) {
 

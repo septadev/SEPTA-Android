1bbfdad0cca34d736a125d835ebc47eabbcb8aee
Antoine Campbell <acampbell@captechventures.com>
2016-08-05 19:27:31 -0400

    - Schedule update - Update build version and database version

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 46;
+    private static final int DATABASE_VERSION = 47;
 
     public SEPTADatabase(Context context) {
 

3c92d872f1afa6098f542f359634117cb2937108
Yannick Baudru <ybaudru@septa.org>
2017-03-15 12:27:19 -0400

    add 311 in TV and kml update

--- app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/databases/SEPTADatabase.java
@@ -19,7 +19,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 56;
+    private static final int DATABASE_VERSION = 61;
 
     public SEPTADatabase(Context context) {
 

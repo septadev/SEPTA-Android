9bc1b26f7ed998c27b9cde61580ff29e40d30aa3
Joseph Kampf <joseph.kampf@gmail.com>
2017-08-28 10:23:24 -0400

    Added support for Crash Analytics.

--- app/src/main/java/org/septa/android/app/MainApplication.java
+++ app/src/main/java/org/septa/android/app/MainApplication.java
@@ -1,13 +1,23 @@
 package org.septa.android.app;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.septa.android.app.database.DatabaseManager;
+import com.crashlytics.android.Crashlytics;
 
 public class MainApplication extends Application {
+    public static final String TAG = MainApplication.class.getSimpleName();
     @Override
     public void onCreate() {
         super.onCreate();
+        if(!BuildConfig.DEBUG) {
+            Crashlytics.start(this);
+            Log.i(TAG, "Starting Crashlytics");
+        } else {
+            Log.i(TAG, "Crashlytics disabled for DEBUG builds");
+        }
+
         // Initialize database, can be moved to logo screen
         DatabaseManager.getInstance(this);
     }

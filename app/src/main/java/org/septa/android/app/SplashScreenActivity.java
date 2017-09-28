359d082fc12ec43bc618b30671abf956b038dafc
Joseph Kampf <joseph.kampf@gmail.com>
2017-09-28 08:42:36 -0400

    Added rotation of images for Splash Screen.

--- app/src/main/java/org/septa/android/app/SplashScreenActivity.java
+++ app/src/main/java/org/septa/android/app/SplashScreenActivity.java
@@ -10,6 +10,7 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 import org.septa.android.app.database.DatabaseManager;
 import org.septa.android.app.services.apiinterfaces.SeptaServiceFactory;
@@ -17,6 +18,8 @@ import org.septa.android.app.services.apiinterfaces.model.Alerts;
 import org.septa.android.app.systemstatus.SystemStatusState;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 import retrofit2.Response;
 
@@ -26,12 +29,25 @@ import retrofit2.Response;
  */
 public class SplashScreenActivity extends AppCompatActivity {
 
+    int[] images = new int[]{R.drawable.bus_image, R.drawable.bg_trolley_image, R.drawable.subway_septa};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView image = (ImageView) findViewById(R.id.splash_image);
+
+        Calendar date = Calendar.getInstance();
+
+        for (int i = images.length - 1; i >= 0; i--) {
+            if (date.get(Calendar.SECOND) % (i + 1) == 0) {
+                image.setImageResource(images[i]);
+                break;
+            }
+        }
+
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {

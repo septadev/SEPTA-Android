cc7d19e794f53bb23a5b5dcd67f2272a6ded2a20
Joseph Kampf <joseph.kampf@gmail.com>
2017-10-03 09:01:55 -0400

    Integrated Connect page with FB and Twitter apps if installed.

--- app/src/main/java/org/septa/android/app/connect/ConnectFragement.java
+++ app/src/main/java/org/septa/android/app/connect/ConnectFragement.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,35 +21,46 @@ import org.septa.android.app.webview.WebViewActivity;
 
 public class ConnectFragement extends Fragment {
 
+    private static final String TAG = ConnectFragement.class.getSimpleName();
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View rootView = inflater.inflate(R.layout.connect_fragment_main, container, false);
 
-        setHttpIntent(rootView, R.id.facebook_arrow, getResources().getString(R.string.facebook_url));
-        setHttpIntent(rootView, R.id.twitter_arrow, getResources().getString(R.string.twitter_url));
+        setAppIntent(rootView, R.id.facebook_arrow, getResources().getString(R.string.facebook_url), getResources().getString(R.string.facebook_app_url));
+        setAppIntent(rootView, R.id.twitter_arrow, getResources().getString(R.string.twitter_url), getResources().getString(R.string.twitter_app_url));
+
         setHttpIntent(rootView, R.id.chat_arrow, getResources().getString(R.string.chat_url), getResources().getString(R.string.chat_title));
         setHttpIntent(rootView, R.id.comment_arrow, getResources().getString(R.string.comment_url), getResources().getString(R.string.comment_title));
 
         return rootView;
     }
 
-    private void setHttpIntent(View rootView, int viewId, final String url) {
+    private void setAppIntent(View rootView, int viewId, final String url, final String appUrl) {
         View twitterLink = rootView.findViewById(viewId);
         twitterLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Activity activity = getActivity();
                 if (activity != null) {
-                    Uri webpage = Uri.parse(url);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                        startActivity(intent);
+                    Uri app = Uri.parse(appUrl);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, app);
+                    if (intent.resolveActivity(getActivity().getPackageManager()) == null) {
+                        Uri webpage = Uri.parse(url);
+                        intent = new Intent(Intent.ACTION_VIEW, webpage);
+                        if (intent.resolveActivity(getActivity().getPackageManager()) == null) {
+                            Log.e(TAG, "Unable to resolve app and website Intent URLs:" + appUrl + " and: " + url);
+                            return;
+                        }
                     }
+
+                    startActivity(intent);
                 }
             }
         });
     }
+
 
     private void setHttpIntent(View rootView, int viewId, final String url, final String title) {
         View twitterLink = rootView.findViewById(viewId);

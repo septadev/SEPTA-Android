ba41a1bf4fbe7c76d81e025aff08a1741a02b49e
Joseph Kampf <joseph.kampf@gmail.com>
2017-11-29 16:49:07 -0500

    Updates to the way that Rail Stations are picked from database.

--- app/src/main/java/org/septa/android/app/database/SEPTADatabase.java
+++ app/src/main/java/org/septa/android/app/database/SEPTADatabase.java
@@ -14,7 +14,7 @@ public class SEPTADatabase extends SQLiteAssetHelper {
     /**
      * Current packaged DB version, update number when packaged DB changes
      */
-    private static final int DATABASE_VERSION = 258;
+    private static final int DATABASE_VERSION = 265;
     private static final String DATABASE_FILE_NAME = "SEPTA.sqlite";
 
     public SEPTADatabase(Context context) {

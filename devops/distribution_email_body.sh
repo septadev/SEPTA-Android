d5acad42b09f30a2096b4243a5eb4f4a63cbe12f
Joseph Kampf <joseph.kampf@gmail.com>
2018-07-19 11:59:34 -0400

    Updated distro list.

--- devops/distribution_email_body.sh
+++ devops/distribution_email_body.sh
@@ -3,7 +3,7 @@ echo "<html> \
 <body> \
 A new SEPTA Android APK is available.<p> \
 The APK can be downloaded from ${accessUrl}</p> \
-Please have them direct all questions or feedback to <bold><a href="mailto:kzinsky@captechconsulting.com">kzinsky@captechconsulting.com</a></bold><p> \
+Please direct all questions or feedback to <bold><a href="mailto:kzinsky@captechconsulting.com">kzinsky@captechconsulting.com</a></bold><p> \
 Git Log:<p> \
 ${git_log} \
 </body> \

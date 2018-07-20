04c1b1ee3522bd19bb72f769edafd52fc827b1f6
Madelyn Luansing <mluansing@captechventures.com>
2018-07-20 16:57:17 -0400

    push notifications can be received with app closed / backgrounded and handled as necessary

--- app/src/main/java/org/septa/android/app/services/apiinterfaces/PushNotificationService.java
+++ app/src/main/java/org/septa/android/app/services/apiinterfaces/PushNotificationService.java
@@ -1,7 +1,7 @@
 package org.septa.android.app.services.apiinterfaces;
 
 import android.app.ActivityManager;
-import android.content.ComponentName;
+import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
@@ -16,10 +16,13 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.septa.android.app.notifications.PushNotificationManager;
 
 import java.util.List;
+import java.util.Map;
 
 public class PushNotificationService extends FirebaseMessagingService {
 
     private static final String TAG = PushNotificationService.class.getSimpleName();
+
+    private static final String NOTIFICATION_KEY_TITLE = "TITLE", NOTIFICATION_KEY_BODY = "BODY";
 
     @Override
     public void onNewToken(String s) {
@@ -42,51 +45,54 @@ public class PushNotificationService extends FirebaseMessagingService {
         });
     }
 
-
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        Log.d(TAG, remoteMessage.getNotification().toString());
-
-        //if the message contains data payload
-        //It is a map of custom keyvalues
-        //we can read it easily
+        Log.d(TAG, remoteMessage.toString());
 
         if (remoteMessage.getData().size() > 0) {
+            Map<String, String> data = remoteMessage.getData();
+
+            // get notification title / body
+            final String title = data.get(NOTIFICATION_KEY_TITLE);
+            final String body = data.get(NOTIFICATION_KEY_BODY);
             // TODO: handle the data message here
-        }
 
-        // get notification title / body
-        final String title = remoteMessage.getNotification().getTitle();
-        final String body = remoteMessage.getNotification().getBody();
+            // check notification subscription window
+            if (PushNotificationManager.isWithinNotificationWindow(getApplicationContext())) {
 
-        // check notification subscription window
-        if (PushNotificationManager.isWithinNotificationWindow(getApplicationContext())) {
+                // display notification
+                if (isAppInForeground(getApplicationContext())) {
 
-            // display notification
-            if (isAppInForeground(getPackageName())) {
-                // show toast if app open
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(new Runnable() {
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Notification: " + body, Toast.LENGTH_LONG).show();
-                    }
-                });
-            } else {
-                // send notification if app backgrounded
-                PushNotificationManager.getInstance(getApplicationContext()).displayNotification(title, body);
+                    // show toast if app open
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Notification: " + body, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    // send notification if app backgrounded
+                    PushNotificationManager.getInstance(getApplicationContext()).displayNotification(title, body);
 
+                }
             }
         }
-
     }
 
-    public boolean isAppInForeground(String packageName) {
-        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> runningTaskInfo = manager.getRunningTasks(1);
-        ComponentName componentInfo = runningTaskInfo.get(0).topActivity;
-        return componentInfo.getPackageName().equals(packageName);
+    private boolean isAppInForeground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND && appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
